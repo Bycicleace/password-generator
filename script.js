f22c48d29ac2886440c4be318cce65b1d829cd81
kvammee <elliott.kvamme@gmail.com>
2021-08-27 16:08:59 -0500

    Added pseudocode workflow to js

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 // Assignment code here
 
+/*
+  PSEUDOCODE
+
+  On button click, do the following:
+  1. Prompt for length of password (number between 8 and 128)
+  2. Confirm using lowercase letters
+  3. Confirm using uppercase letters
+  4. Confirm using numbers
+  5. Confirm using special characters
+  6. Generate random password based on answers above.
+  7. Display password in the box.
+
+*/
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
